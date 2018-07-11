3de1874cbf2d0e761683a13541beeff44f9ebb17
HunterOfBugs <39541389+HunterOfBugs@users.noreply.github.com>
2018-07-11 17:22:39 -0500

    Create host.py

---
+++ 24/24/host.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+token = "NDY2NDI0ODU0OTU0OTAxNTI0.Dib3pw.ym4-aG20eWkHoASTj15bDJl_C2Y"
+client = commands.Bot(command_prefix = '()')
+
+client.run(token)
